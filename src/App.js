9768e3774eae3165d3a8baa25382381918f35c67
Josué Salazaku <josuesalazaku@gmail.com>
2024-02-19 14:06:52 +0100

    Create HeaderComponents

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import HeaderComponents from '/Users/josuesalazaku/Developer/BeCode/Summerpastures/verou-5-react-intro-josuesalazaku/src/Components/HeaderComponents.js'
+
+
+function App() {
+  return (
+    <div className="App h-screen bg-zinc-900 flex flex-col">
+      <HeaderComponents/>
+      <main className='flex flex-col justify-center items-center h-full'>
+        <h1 className='text-white text-6xl pb-20'>My to do's</h1>
+
+      </main>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/HeaderComponents.js
@@ -0,0 +1,14 @@
+function App() {
+    return (
+      <header className="App-header">
+        <ul className="text-white font-semibold flex felx-row justify-around pt-10">
+          <li>Logo</li>
+          <li>Todo's</li>
+          <li>Logs's</li>
+        </ul>
+      </header>
+    );
+  }
+  
+  export default App;
+  
