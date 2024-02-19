1b4630e39aa18962ec896834c1f98fb0da97870a
Josué Salazaku <josuesalazaku@gmail.com>
2024-02-19 14:59:30 +0100

    Add todolist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import HeaderComponents from '/Users/josuesalazaku/Developer/BeCode/Summerpastures/verou-5-react-intro-josuesalazaku/src/Components/HeaderComponents.js'
 import FooterComponents from '/Users/josuesalazaku/Developer/BeCode/Summerpastures/verou-5-react-intro-josuesalazaku/src/Components/FooterComponents.js'
-import TodoComponents from '/Users/josuesalazaku/Developer/BeCode/Summerpastures/verou-5-react-intro-josuesalazaku/src/Components/TodoComponents.js'
+import Todolist from './Components/Todolist.js'
 
 
 function App() {
@@ -10,7 +10,14 @@ function App() {
       <main className='flex flex-col justify-center items-center h-full'>
         <h1 className='text-white text-6xl pb-20'>My to do's</h1>
         
-      <TodoComponents/>
+        <Todolist />
+        <>
+        <div className="flex justify-evenly items-center space-x-3">
+             <input type="text" className="rounded-md h-8 w-52 p-2" />
+             <button className="my-5 p-2 rounded-md font-bold text-white border border-green-600">ADD</button>
+             <button className="my-5 p-2 rounded-md font-bold text-white border border-red-600">DELETE</button>
+       </div>
+        </>
       </main>
       <FooterComponents/>
 
