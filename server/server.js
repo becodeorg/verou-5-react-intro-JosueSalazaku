e3a90465e7956baad6e756ab9b16a2af77fbcfe1
Josué Salazaku <josuesalazaku@gmail.com>
2024-02-22 10:56:44 +0100

    Add env file for database setup

--- server/server.js
+++ server/server.js
@@ -7,7 +7,18 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
+ 
+let todos = [];
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
